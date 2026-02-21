3bc3a7ddc4250e701dc1f5d58ffb55c95fd6bbc2
AtheoJester23 <atheojester@gmail.com>
2026-02-21 12:01:13 +0800

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='h-[35px] py-1 px-3  text-white bg-gradient-to-t from-[rgb(77,77,77)] to-[rgb(85,85,85)]'>
+      <button className='gradientBtn leading-none px-3 py-1 rounded text-sm'>File</button>
+    </div>
+  )
+}
+
+export default Navbar
